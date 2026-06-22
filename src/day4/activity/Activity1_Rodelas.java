afd71e9c0d669cf5867b73621915c763091698a2
Geru <113689254+Gelatino0308@users.noreply.github.com>
2026-06-22 17:39:59 +0800

    add discussion and activity codes for day4

---
+++ src/day4/activity/Activity1_Rodelas.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day4.activity;
+
+/**
+ *
+ * @author Angelo
+ */
+public class Activity1_Rodelas {
+    public static void main (String[] args) {
+        
+        // data types
+        boolean isEnrolled = true;
+        byte luckyNum = 8;
+        short monthlyBudget = 15_000;
+        int population = 1_200_000;
+        long jackpotPrize = 5_000_000;
+        
+        // running speed in kilometers per hour
+        float runningSpeedKMPH = 20.5f;
+       
+        // size of bacteria in meters
+        double bacteriaSizeM = 0.000005;
+        char initial = 'S';
+        String IDE = "NetBeans";
+        
+        // print statements
+        System.out.println("boolean isEnrolled: " + isEnrolled);
+        System.out.println("byte luckyNum: " + luckyNum);
+        System.out.println("short monthlyBudget: " + monthlyBudget);
+        System.out.println("int population: " + population);
+        System.out.println("long jackpotPrize: " + jackpotPrize);
+        System.out.println("float runningSpeedKMPH: " + runningSpeedKMPH);
+        System.out.println("double bacterialSizeM: " + bacteriaSizeM);
+        System.out.println("char initial: " + initial);
+        System.out.println("String IDE: " + IDE);
+    }
+}
---
+++ src/day4/discussion/Variable.java
@@ -0,0 +1,45 @@
+
+package day4.discussion;
+
+
+public class Variable {
+    
+    public static void main (String[] args) {
+        
+        // Mini-activity
+        // Requirements: Create a variable for firstName,
+        // middleInitial, lastName and assign values to it.
+        // Expected Output: Angelo S. Rodelas
+        
+        String firstName = "Angelo ", middleInitial = "S. ", lastName = "Rodelas";
+        
+        System.out.println("Full Name: " + firstName + middleInitial + lastName);
+        
+        
+        // initialization
+        // DT   var     value
+        String timba = "tubig";
+        
+        timba = "bigas";
+        
+        // Printing a variable
+        // System.out.println(timba);
+        
+        // 23, 24
+        int age = 23;
+        int age2 = 24;
+        
+        // System.out.println("Age: " + age);
+        
+        // 26, 27
+        int age3 = 26, age4 = 27;
+        
+        // 38.5
+        // declaration
+        double temp;
+        
+        // assignment
+        temp = 38.5;
+        // System.out.println("Temp: " + temp);
+    }
+}
